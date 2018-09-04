782abd285b70de8e6f1130606b3c2d0b514df891
Joan Felez <joan.felez@movicoders.com>
2018-09-04 16:29:43 +0200

    Cleaning the repo

---
+++ Nodepop/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "google",
+    "parserOptions": {
+        "ecmaVersion": 6
+    },
+    "rules":{
+        "max-len": ["error", { "code": 140 }],
+        "require-jsdoc": 0
+    }
+};
